b8a4e20c4fb4e25198d54e177afbb51c0ee7b365
Darian <darianychen@gmail.com>
2023-02-12 16:48:26 -0500

    Add css reset, create splash page, delete react photo

---
+++ src/Components/SplashPage/index.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../NavBar";
+import FooterComponent from "../Footer";
+
+const SplashPage = () => {
+
+    return(
+        <div>
+            <NavBar/>
+            <FooterComponent/>
+        </div>
+    )
+
+}
+
+export default SplashPage;
