94fb1fdde18c4d449f9a867e8ddcc83ebeeb7efc
Darian <darianychen@gmail.com>
2023-03-23 23:06:40 -0400

    Add restaurant description to splash page and change slider css

--- src/Components/SplashPage/index.jsx
+++ src/Components/SplashPage/index.jsx
@@ -1,6 +1,8 @@
 import NavBar from "../NavBar";
 import FooterComponent from "../Footer";
 import Slider from "../Slider";
+import './index.css';
+import restaurantImage from "../../assets/Images/restaurant-image.jpeg";
 
 const SplashPage = () => {
 
@@ -8,9 +10,20 @@ const SplashPage = () => {
         <div>
             <NavBar/>
             <Slider/>
+
+            <div className="restaurant-info">
+                <div className="restaurant-image">
+                    <img src={restaurantImage} alt="Restaurant Image"/>
+                </div>
+                <div className="restaurant-description">
+                    <h2>About Jing Fong</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id odio id orci fringilla fringilla. Nullam dictum elit in mi laoreet, sed bibendum magna iaculis. Duis id risus ac lacus semper fringilla. In suscipit sapien sit amet lobortis convallis. Suspendisse bibendum nulla vitae rutrum posuere.</p>
+                </div>
+            </div>
+
             <FooterComponent/>
         </div>
-    )
-}
+    );
+};
 
 export default SplashPage;
