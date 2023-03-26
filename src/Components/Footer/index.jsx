1de66ff329a31f4d70e1ae36e1fc1721bf8af9e9
Darian <darianychen@gmail.com>
2023-03-26 15:59:28 -0400

    Update footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,6 +1,13 @@
 import './index.css';
+import Logo from './logo.jsx';
 
-const FooterComponent = () => {
+const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0, 
+            behavior: 'smooth'
+        });
+    };
 
     return (
         <footer className="footer">
@@ -12,10 +19,9 @@ const FooterComponent = () => {
                     Littleton, NH 03561
                     <br />
                     <br />
-                    Telephone: 
-                    <a href='tel:6034446688'>(603) 444-6688</a>
+                    T: <a href='tel:6034446688'>(603) 444-6688</a>
                     <br />
-                    <a href='tel:6034446288'>(603) 444-6288</a>
+                    T: <a href='tel:6034446288'>(603) 444-6288</a>
                 </p>
             </div>
             <div className="footer-section">
@@ -28,14 +34,11 @@ const FooterComponent = () => {
                     Sun: 11am - 10pm
                 </p>
             </div>
-            <div className="footer-section">
-                <h3 className="footer-section-header">Map</h3>
-                <div className="map-container">
-                    <iframe title="Jing Fong Location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2855.158221168869!2d-71.77453988449348!3d44.30669771794461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4056bfeabf53e8ed%3A0xf0641a0554961a3b!2sJing%20Fong!5e0!3m2!1sen!2sus!4v1679688364395!5m2!1sen!2sus" width="100%" height="200" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
-                </div>
+            <div className="footer-section" onClick={scrollToTop}>
+                <Logo />
             </div>
         </footer>
     );
 };
 
-export default FooterComponent;
+export default Footer;
