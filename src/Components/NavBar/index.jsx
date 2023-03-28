de905260d4af15ba3ca07fa4500241e6641be510
Darian <darianychen@gmail.com>
2023-03-28 19:02:50 -0400

    Make responsive nav close on click and change map css

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -9,16 +9,20 @@ const NavBar = () => {
         navRef.current.classList.toggle("responsive_nav");
     };
 
+    const closeNavBar = () => {
+        navRef.current.classList.remove("responsive_nav");
+    };
+
     return(
         <header id='main-header'>
             <a href='/jing-fong/#/'>
             <h3 id="jing-fong">Jing Fong <span>金豐</span></h3>
             </a>
             <nav ref={navRef}>
-                <a href="/jing-fong/#/" className='link'>Home</a>
-                <a href="/jing-fong/#/menu">Menu</a>
-                <a href="/jing-fong/#/gallery">Gallery</a>
-                <a href="/jing-fong/#/feedback">Feedback</a>
+                <a href="/jing-fong/#/" className='link' onClick={closeNavBar}>Home</a>
+                <a href="/jing-fong/#/menu" onClick={closeNavBar}>Menu</a>
+                <a href="/jing-fong/#/gallery" onClick={closeNavBar}>Gallery</a>
+                <a href="/jing-fong/#/feedback" onClick={closeNavBar}>Feedback</a>
                 <button className='nav-btn nav-close-btn' onClick={toggleNavBar}>
                     <FaTimes/>
                 </button>
