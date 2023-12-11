5672f317b5b4b73c8e4d49176f75d10aa31ee0d8
Darian <darianychen@gmail.com>
2023-12-10 20:27:10 -0500

    Fix path in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import SplashPage from './Components/SplashPage';
 import Menu from './Components/Menu';
 import './App.css';
 import Footer from './Components/Footer';
-import Feedback from './Components/Feedback';
-import Gallery from './Components/Gallery';
 
 function App() {
   return (
     <>
-      <NavBar />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<SplashPage />} />
-          <Route path="/menu" element={<Menu />} />
-          {/* <Route path="/feedback" element={<Feedback />} />
-          <Route path="/gallery" element={<Gallery />} /> */}
-        </Routes>
-      </HashRouter>
-      <Footer />
-    </>
+    <NavBar />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<SplashPage />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </HashRouter>
+    <Footer />
+  </>
   );
 }
 
